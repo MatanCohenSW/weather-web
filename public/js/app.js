990eb60939baa861983eb114fa460cace46cf993
Matan Cohen <matancohensw@gmail.com>
2024-06-21 20:25:50 +0300

    remove logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("client side loaded");
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const responseMessage = document.getElementById('response-message')
