c3a5109784bb9229f831dce54708055cff31db6f
Matan Cohen <matancohensw@gmail.com>
2024-06-26 23:17:22 +0300

    commit to upload

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    redis: {
+        password: 'eIFQ2QDQjKRmXdcjtYufcAPIiIVZJDny',
+        host: 'redis-11653.c256.us-east-1-2.ec2.redns.redis-cloud.com',
+        port: 11653
+    },
+    weatherstack:{
+        accessKey:'9e48245b8021a72c154b9ca8b5a26ee6'
+    }
+};
