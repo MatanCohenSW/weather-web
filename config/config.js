97ff06a757dfaea85be2fb80bd02a0e006ac5c59
Matan Cohen <matancohensw@gmail.com>
2024-06-26 22:10:05 +0300

    commit

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    redis: {
-        password: 'eIFQ2QDQjKRmXdcjtYufcAPIiIVZJDny',
-        host: 'redis-11653.c256.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 11653
-    }
-};
